d46e5aef457e0d2bcc2f4df0aac835ceda10667e
Павел Васильев <pahan0595@gmail.com>
2023-06-10 15:03:46 +0300

    Закончил 4-ю задачу

---
+++ Задача 4.py
@@ -0,0 +1,8 @@
+a = int(input())
+Kate = a/2 
+Both_boys = Kate / 2
+
+if a % 2 == 0:
+    print(f'Катя сделала {Kate} журавликов, а Петя и Сережа сделали по {Both_boys / 2} журавликов')
+else:
+    print('Кто-то точно сачковал')
